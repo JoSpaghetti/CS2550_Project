f0802a7cfd1281803997879df5980f73d3170a42
JoSpaghetti <143671979+JoSpaghetti@users.noreply.github.com>
2025-05-10 00:25:13 -0700

    Connect Database to Map GUI & add labels to pop-ups

---
+++ database/database.py
@@ -0,0 +1,240 @@
+import pandas as pd
+import duckdb
+import geopy
+from geopy import distance
+
+class LocationDatabase:
+    connection = duckdb.connect(':memory:')
+    df_name = "locations_df"
+    database_name = "location_table"
+
+    def __init__(self, folder=""):
+        self.folder = folder
+        museum = self.__start_museums_aquarium_zoo_dataframe()
+        fast_food = self.__fast_food_restaurant_dataframe()
+        hospital = self.__us_hospital_locations_dataframe()
+
+        #dataframe for all location types
+        locations_df = pd.concat([museum, fast_food, hospital], ignore_index=True, sort=False)
+        #print(locations_df)
+
+        #referenced from DuckDB documentation and https://medium.com/@anshubantra/using-duckdb-in-python-a-comprehensive-guide-d14bc0b06546
+
+        #create table
+        self.connection.sql(f"CREATE OR REPLACE TABLE {self.database_name} AS SELECT * FROM {self.df_name}")
+
+        #insert elements into table
+        self.connection.sql(f'INSERT INTO {self.database_name} SELECT * FROM {self.df_name}')
+
+
+    def nearby_locations(self, longitude1, latitude1, distance_range, units="km"):
+        """Given a set of coordinate values and a distance, it returns a series of location values
+        :param longitude1: Longitude of first location
+        :param latitude1: Latitude of first location
+        :param distance_range: Range of distance in meters
+        :param units: the unit of distance measure"""
+
+        #if one set of longitude and a distance are given
+        start_point = (latitude1, longitude1)
+        bounding_box = dict()
+        for degree in [0,90,180,270]:
+            if units == "mi":
+                distance_length = geopy.distance.distance(miles = distance_range/2).destination(point = start_point,
+                                                                                                bearing=degree)
+                bounding_box[degree] = distance_length
+
+
+            if units == "km":
+                distance_length = geopy.distance.distance(kilometers=distance_range/2).destination(point=start_point,
+                                                                                                   bearing=degree)
+                bounding_box[degree] = distance_length
+
+        #find between bounding_box[0] and bounding_box[180]
+        top_bound = bounding_box[0].latitude
+        right_bound = bounding_box[90].longitude
+        bottom_bound = bounding_box[180].latitude
+        left_bound = bounding_box[270].longitude
+
+        #print(f"\n{bounding_box}\n{top_bound},{left_bound},{bottom_bound},{right_bound}\n")
+
+        result = self.connection.sql(f"SELECT * FROM {self.database_name} "
+                                     f"WHERE Longitude BETWEEN {left_bound} AND {right_bound} "
+                                     f"AND Latitude BETWEEN {bottom_bound} AND {top_bound}")
+
+        return result.fetchall()
+
+    @staticmethod
+    def distance_between(longitude1, latitude1, longitude2, latitude2, units="km"):
+        """Uses the Haversine formula in the geopy.distance library to find the difference between two coordinate points
+        :param longitude1: Longitude of first point
+        :param latitude1: Latitude of first point
+        :param longitude2: Longitude of second point
+        :param latitude2: Latitude of second point
+        :param units: The unit of measurement used ('km' or 'mi')"""
+
+        point1 = (longitude1, latitude1)
+        point2 = (longitude2, latitude2)
+        total_distance = 0
+
+        if units == "mi": #miles
+            total_distance = geopy.distance.distance(point1, point2).miles
+        elif units == "km": #kilometers
+            total_distance = geopy.distance.distance(point1, point2).km
+        else: #any other unit of measurement
+            pass
+
+        return total_distance
+
+    def location_search(self, name=None, location_type = None, address=None, latitude=None, longitude=None, state=None, zipcode=None ):
+        """Given a name, address, latitude, OR longitude, it returns a list of tuples.
+        The tuple list included the name, type, longitude, latitude, and address of the location.
+        :param name: name of the location
+        :param location_type: type of location
+        :param address: address of the location
+        :param latitude: latitude of the location
+        :param longitude: longitude of the location
+        :param state: state of the location
+        :param zipcode: zipcode of the location"""
+
+        if name is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE Name = '{name}'")
+            # fetchall returns the info in a list of tuples
+            return result.fetchall()
+
+        if location_type is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE Type = '{location_type}'")
+            return result.fetchall()
+
+        if address is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE Address = '{address}'")
+            return result.fetchall()
+
+        if latitude is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE Latitude = '{latitude}'")
+            return result.fetchall()
+
+        if longitude is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE Longitude = '{longitude}'")
+            return result.fetchall()
+
+        if state is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE State = '{state}'")
+            return result.fetchall()
+
+        if zipcode is not None:
+            result = self.connection.sql(f"SELECT * FROM {self.database_name} WHERE ZipCode = '{zipcode}'")
+            return result.fetchall()
+
+        return None
+
+    def __start_museums_aquarium_zoo_dataframe(self):
+        """an initializer for the zoo_dataframe"""
+        # Download latest version
+        path = f"{self.folder}museums.csv"
+
+        # initialize dataframe from csv file
+        museum = pd.read_csv(path,
+                             usecols=["Museum Name", "Street Address (Administrative Location)", "Museum Type",
+                                      "City (Administrative Location)", "State (Administrative Location)",
+                                      "Zip Code (Administrative Location)", "Latitude", "Longitude"],
+                             )
+
+        museum = museum.rename(columns={"Museum Name": "Name",
+                                        "Museum Type": "LocationType",
+                                        "Street Address (Administrative Location)": "Address",
+                                        "City (Administrative Location)": "City",
+                                        "State (Administrative Location)": "State",
+                                        "Zip Code (Administrative Location)": "ZipCode",
+                                        })
+
+        # adds a new column called countries with every entry being "USA"
+        museum["Country"] = ["USA" for _ in range(len(museum))]
+
+        # reformats the column order to allow for concatenation
+        museum = museum[
+            ["Name", "LocationType", "Latitude", "Longitude", "Address", "City", "State", "Country", "ZipCode"]]
+
+        return museum
+
+
+    def __us_hospital_locations_dataframe(self):
+        path = f"{self.folder}us_hospital_locations.csv"
+
+        # initialize dataframe from csv file
+        hospital = pd.read_csv(path,
+                               usecols=["NAME", "ADDRESS", "TYPE", "CITY", "STATE", "ZIP", "LATITUDE", "LONGITUDE"],
+                               )
+        hospital = hospital.rename(columns={"NAME": "Name",
+                                            "ADDRESS": "Address",
+                                            "TYPE": "LocationType",
+                                            "CITY": "City",
+                                            "STATE": "State",
+                                            "ZIP": "ZipCode",
+                                            "LATITUDE": "Latitude",
+                                            "LONGITUDE": "Longitude", })
+
+        # adds a new column called countries with every entry being "USA"
+        hospital["Country"] = ["USA" for _ in range(len(hospital))]
+
+        # reformats the column order to allow for concatenation
+        hospital = hospital[
+            ["Name", "LocationType", "Latitude", "Longitude", "Address", "City", "State", "Country", "ZipCode"]]
+
+        return hospital
+
+
+    def __fast_food_restaurant_dataframe(self):
+        path = f"{self.folder}Fast_Food_Restaurants_US.csv"
+
+        # initialize dataframe from csv file
+        fast_food = pd.read_csv(path,
+                                usecols=["name", "address", "city", "categories", "latitude", "longitude", "postalCode",
+                                         "province"],
+                                )
+
+        fast_food = fast_food.rename(columns={"name": "Name",
+                                              "address": "Address",
+                                              "city": "City",
+                                              "categories": "LocationType",
+                                              "longitude": "Longitude",
+                                              "latitude": "Latitude",
+                                              "postalCode": "ZipCode",
+                                              "province": "State"})
+
+        # adds a new column called countries with every entry being "USA"
+        fast_food["Country"] = ["USA" for _ in range(len(fast_food))]
+
+        # reformats the column order to allow for concatenation
+        fast_food = fast_food[
+            ["Name", "LocationType", "Latitude", "Longitude", "Address", "City", "State", "Country", "ZipCode"]]
+
+        return fast_food
+
+
+def main():
+    location_db = LocationDatabase()
+
+    sonic_drive_in = location_db.location_search(name = "SONIC Drive In")
+    print (f"Sonic Info: {sonic_drive_in.__sizeof__()}")
+    print (sonic_drive_in)
+
+    california_locations = location_db.location_search(state = "CA")
+    print (f"California Info: {california_locations.__sizeof__()}")
+
+    test = location_db.location_search(longitude=-121.83653)
+    print (test, end="\n\n")
+
+    for i in range(10):
+        print (california_locations[i])
+
+    nearby_local = location_db.nearby_locations(longitude1=-121.83653,
+                                                latitude1=39.7254,
+                                                distance_range=10000000,
+                                                units="mi",
+                                                )
+
+    print (nearby_local)
+
+
+if __name__ == "__main__":
+    main()
--- videomap.py
+++ videomap.py
@@ -1,32 +1,62 @@
-import pandas as pd
 import folium
 from folium.plugins import LocateControl
+from database.database import LocationDatabase #local database
 
-# Load the CSV file
-file = pd.read_csv('Fast_Food_Restaurants_US.csv')
+"""
+database return structure:
+[("Name", "LocationType", "Latitude", "Longitude", "Address", "City", "State", "Country", "ZipCode"),]
+"""
 
-# Create a basic map centered at the first location in the dataset
-latitude = file['latitude'].iloc[0]
-longitude = file['longitude'].iloc[0]
-map = folium.Map(location=[latitude, longitude], zoom_start=10)
+def new_map(longitude, latitude):
+    # Load the database file
+    local_db = LocationDatabase("database/")
+    print ("Database Initialized")
 
-# Add markers for each restaurant
-locations = []
-for index, row in file.iterrows():
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row.get('name', 'Fast Food Restaurant')  # Replace 'categories' with the actual column for names
-    ).add_to(map)
-    locations.append((row.get('name', 'Fast Food Restaurant'), row['latitude'], row['longitude']))
+    # get a list of locations near a point
+    nearby_locations = local_db.nearby_locations(longitude1=longitude,
+                                                 latitude1=latitude,
+                                                 distance_range=75,
+                                                 units="mi",
+                                                 )
+    print ("Nearby Locations Generated")
 
-# Add a locator button to the map
-LocateControl().add_to(map)
+    # Create a basic map centered at the location parameters passed into the method
+    latitude = nearby_locations[0][2]
+    longitude = nearby_locations[0][3]
+    gps_map = folium.Map(location=[latitude, longitude],
+                         zoom_start=12,
+                         control_scale=True)
 
+    # Add a locator button to the map
+    folium.plugins.LocateControl(auto_click=True,
+                                 flyTo=True,
+                                 ).add_to(gps_map)
 
-# Add the dropdown and script to the map
-map.get_root().html.add_child(folium.Element(dropdown_html + dropdown_script))
+    print ("Basic Map Generated")
 
-# Save the map to an HTML file
-map.save('fast_food_map.html')
+    # Add markers for each restaurant
+    locations = []
+    for location in nearby_locations:
+        text = f"{location[0]}:\n {location[4]}, {location[5]}, {location[6]}, {location[7]}, {location[8]} "
+        folium.Marker(
+            location=[location[2], location[3]],
+            tooltip=location[1],
+            popup=text,
+            max_width=200
+        ).add_to(gps_map)
+        locations.append(location)
 
-print("Map has been created and saved as 'fast_food_map.html'.")
+    print ("Location Markers Placed")
+
+    # Add the dropdown and script to the map
+    # gps_map.get_root().html.add_child(folium.Element(dropdown_html + dropdown_script))
+
+    # Save the map to an HTML file
+    filename = 'gps_map.html'
+    gps_map.save(filename)
+
+    print(f"Map has been created and saved as {filename}.")
+
+if __name__ == "__main__":
+    new_map(-117.821667, 34.056389)
+
